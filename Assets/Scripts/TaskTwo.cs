777bbc41cc445f4642a06af502c4c853bac8a1c1
Colin Pierce <cpierce192004@gmail.com>
2026-01-29 11:53:01 -0500

    Updated Script Two

--- Assets/Scripts/TaskTwo.cs
+++ Assets/Scripts/TaskTwo.cs
@@ -2,6 +2,13 @@ using UnityEngine;
 
 public class TaskTwo : MonoBehaviour
 {
+
+    // Variable Declaration
+    public double coverPrice = 2; //Price consumers pay
+    public int bookAmount = 2; //Amount of books purchases
+    public double customerPrice; //product 
+    public double bookstoreDiscountedPrice; //
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,5 +19,31 @@ public class TaskTwo : MonoBehaviour
     void Update()
     {
         
+    }
+
+    //Create the amount the consumers paid
+    public void consumerPrice ()
+    {
+        Debug.LogFormat("You have input ", bookAmount, " books that cost ", coverPrice, " to consumers individually.");
+        customerPrice = coverPrice * bookAmount;
+        Debug.LogFormat("The total amount of money earned from consumers is: ", customerPrice);
+    }
+
+    //Give discount to bookstore
+    public void bookstoreDiscount()
+    {
+        double bookstoreDiscountedPrice = customerPrice * 0.4;
+    }
+
+    //Create shipping cost
+    public void shippingCost ()
+    {
+
+    }
+
+    //Compare the price consumers paid to the price the bookstore paid
+    public void profitComparison ()
+    {
+
     }
 }
